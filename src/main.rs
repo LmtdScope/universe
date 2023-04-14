ee57f4ac2c9ee6f7b6d10e4dee51e6eedf3bda86
LmtdScope <107701452+LmtdScope@users.noreply.github.com>
2023-04-14 15:46:45 -0600

    particles traced over noised field

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use nannou::{prelude::*};
+use nannou::{prelude::*, noise::{Perlin, NoiseFn}};
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 struct Thing{
-    position: Vec2
+    positions: Vec<Vec2>
 }
 impl Thing {
     pub fn new (p:Vec2) -> Self{
+        let mut positions = Vec::new();
+        positions.push(p);
         Thing{
-            position:p,
+            positions,
         }
     }
 }
 struct Model {
    things:Vec<Thing>,
+   noise: Perlin,
 }
 
 const N_THINGS: usize = 2000;
@@ -27,31 +30,51 @@ fn model(app: &App) -> Model {
     for i in 0 .. N_THINGS{
     let thing = Thing::new(vec2(
         (random::<f32>()-0.5)*1024.0,
-        (random::<f32>()-0.5)*1024.0,
+        (random::<f32>()-0.5)*1024.0
         ));
     things.push(thing);}
 
-    Model { things, }
+    let noise = Perlin::new();
+
+    Model { 
+        things,
+        noise, }
     
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let time = app.elapsed_frames() as f32 / 120.0;
+    let sn = 0.01 + time.cos() as f64 * 0.005;
 
 for thing in model.things.iter_mut(){ 
-    thing.position += vec2(1.0,0.0);
-     }
-}
 
+    thing.positions.clear();
+    thing.positions.push(vec2(
+        (random::<f32>()-0.5)*1024.0,
+        (random::<f32>()-0.5)*1024.0));
+
+    for k in 0 .. 50{
+    let last = thing.positions[0];
+    let new = last + vec2( 
+        model.noise.get([sn*last.x as f64, sn*last.y as f64, 0.0]) as f32,
+        model.noise.get([sn*last.x as f64,sn*last.y as f64, 1.0]) as f32,
+    );
+thing.positions.insert(0, new);
+}
+}
+}
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
-    let time = app.elapsed_frames() as f32 / 60.0;
-
+   
+    if app.elapsed_frames() == 1{
     draw.background().color(BLACK);
- 
+    }
 for thing in model.things.iter(){
-    draw.ellipse().xy(thing.position).radius(5.0).color(WHITE);
+    draw.polyline().points(thing.positions.iter().cloned()).color(WHITE);
+   
 }
+draw.rect().w_h(1024.0, 1024.0).color(srgba(0.0,0.0,0.0,0.1));
 
     draw.to_frame(app, &frame).unwrap();
 }
